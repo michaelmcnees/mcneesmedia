6b778f49c7e8c2470f2e6c8fd2a191dce02f3919
Michael McNees <michael.mcnees@hygieia.com>
2025-01-18 22:31:27 -0500

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
-import './globals.css'
+
+import { Analytics } from '@vercel/analytics/react'
+
 import { Header } from '@/components/header'
 import { Footer } from '@/components/footer'
+
+import './globals.css'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -33,6 +37,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </div>
+        <Analytics />
       </body>
     </html>
   )
